f0b179d04eb8bcc19b516ab2149620d2e56afd5e
Jacob Larson <hockeybobber@comcast.net>
2023-11-02 10:48:26 -0500

    set up routes on server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
+
+//make random = 0 if it doesnt reset
+function makeRandom(min, max) {
+  return Math.floor(Math.random()* max) + min;
+}
+
+let roundNum = 0;
+
+//console.log(makeRandom(1, 25));
+
+let randomNumber = makeRandom(1, 25);
+
+console.log('the random number is:', randomNumber);
+
+const guesses = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
+app.use(express.json())
+
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+
+app.get('/guesses', (req, res) => {
+  console.log('we got a get req');
+  res.send(guesses);
+})
+
+app.post('/guesses', (req, res) => {
+  console.log('we got POST req');
+  let newGuesses = req.body
+  guesses.push(newGuesses)
+  res.sendStatus(201)
+  roundNum++
+})
 
 
 app.listen(PORT, () => {
